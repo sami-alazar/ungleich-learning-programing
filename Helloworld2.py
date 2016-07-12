bbeeaf69a105a830b36fe5135d23da381aca7f0b
Yourname <Yourname@gmail.com>
2016-07-12 13:50:04 +0200

    Updated python2 version of helloworld

--- Helloworld2.py
+++ Helloworld2.py
@@ -1,6 +1,1 @@
-Python 2.7.6 (default, Jun 22 2015, 17:58:13) 
-[GCC 4.8.2] on linux2
-Type "copyright", "credits" or "license()" for more information.
->>> print "hello world"
-hello world
->>> 
+print "hello world"
