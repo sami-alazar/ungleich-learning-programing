51f3e717df344c11dd5b56a1d13a545507a61ebc
Yourname <Yourname@gmail.com>
2016-07-13 18:04:01 +0200

    Schweiz in python

---
+++ capitals.py
@@ -0,0 +1,33 @@
+Zurich= 389000 
+Bern=1009418
+Luzern=77000
+Uri=9000
+Schwyz=14000
+Obwalden=10000
+Nidwalden=42080
+Glarus=39794
+Zug=26000
+Wallis=30000
+Vaud=128000
+Ticino=17000
+Thurgau=23000
+Solothurn=16000
+Schaffhausen=35000
+St_Gallen=73000
+Neuchatel=33000
+Jura= 12000
+Graubunden=34000
+Genf=191000
+Freiburg=35000
+Basel_Stadt=169000
+Basel_land=13000
+Appenzell_Innerrhoden=6000 
+Appenzell_Ausserrhoden=15000
+Aargau=20000
+Kanton=26
+
+sumall=Zurich + Bern + Luzern + Uri + Schwyz + Obwalden+ Nidwalden + Glarus + Zug + Wallis + Vaud + Ticino + Thurgau + Solothurn + Schaffhausen + St_Gallen + Neuchatel + Jura + Graubunden + Genf + Freiburg + Basel_Stadt + Basel_land + Appenzell_Innerrhoden + Appenzell_Ausserrhoden + Aargau
+
+print sumall
+average= sumall / Kanton 
+print "the average of the", Kanton ," capital cities of switzerland is",average
